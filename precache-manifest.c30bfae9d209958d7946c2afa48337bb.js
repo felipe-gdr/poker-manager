3060762dafd457879f69ef6a08643b548ec9160c
Felipe Reis <felipe.gdr@gmail.com>
2020-05-25 19:17:20 +1000

    Updates

---
+++ precache-manifest.c30bfae9d209958d7946c2afa48337bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8f865d7e78ca6321cf4fc5400f4020",
+    "url": "/poker-manager/index.html"
+  },
+  {
+    "revision": "075282808a6adac74a41",
+    "url": "/poker-manager/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "260d135e8d8b717bf145",
+    "url": "/poker-manager/static/js/2.9598ca2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/poker-manager/static/js/2.9598ca2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075282808a6adac74a41",
+    "url": "/poker-manager/static/js/main.97084d75.chunk.js"
+  },
+  {
+    "revision": "b617155a89022b8fde40",
+    "url": "/poker-manager/static/js/runtime-main.84df4aaf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/poker-manager/static/media/logo.5d5d9eef.svg"
+  }
+]);
